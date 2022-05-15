393237338a63d07fd82b93cd45b0987eaa1125b7
brainfoolong <brainfoolong@users.noreply.github.com>
2022-05-15 14:13:20 +0200

    updates for github actions tests with framelix-docker instead of own old solution

--- phpstan.sh
+++ phpstan.sh
@@ -1,1 +1,3 @@
+#!/bin/sh -e
+
 docker-compose exec -T phpfpm bash -c "cd /framelix && php composer.phar run phpstan"
--- phpunit.sh
+++ phpunit.sh
@@ -1,5 +1,7 @@
+#!/bin/sh -e
+
 # fix corrupt db in case tests was interrupted
 docker-compose exec -T db bash -c "mysql -u app -papp -e 'create database if not exists app'"
 
 # run php unit
-exec docker-compose exec -T phpfpm bash -c "cd /framelix && cp /framelix/config/php.ini /opt/bitnami/php/etc/conf.d/xdebug.ini && php vendor/phpunit/phpunit/phpunit --coverage-clover clover.xml --bootstrap modules/FramelixTests/tests/_bootstrap.php --configuration  modules/FramelixTests/tests/_phpunit.xml"
+docker-compose exec -T phpfpm bash -c "cd /framelix && cp /framelix/config/php.ini /opt/bitnami/php/etc/conf.d/xdebug.ini && php vendor/phpunit/phpunit/phpunit --coverage-clover clover.xml --bootstrap modules/FramelixTests/tests/_bootstrap.php --configuration  modules/FramelixTests/tests/_phpunit.xml"
