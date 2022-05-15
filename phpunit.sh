4c7e5336d7fe2cfdc936052fc2bbbb8a7b129fcb
brainfoolong <brainfoolong@users.noreply.github.com>
2022-05-15 14:50:39 +0200

    updates for github actions tests with framelix-docker instead of own old solution

--- phpunit.sh
+++ phpunit.sh
@@ -4,4 +4,4 @@
 docker-compose exec -T db bash -c "mysql -u app -papp -e 'create database if not exists app'"
 
 # run php unit
-docker-compose --log-level DEBUG exec -T phpfpm bash -c "cd /framelix && cp /framelix/config/php.ini /opt/bitnami/php/etc/conf.d/xdebug.ini && php vendor/phpunit/phpunit/phpunit --coverage-clover clover.xml --bootstrap modules/FramelixTests/tests/_bootstrap.php --configuration  modules/FramelixTests/tests/_phpunit.xml"
+docker-compose exec -T phpfpm bash -c "cd /framelix && cp /framelix/config/php.ini /opt/bitnami/php/etc/conf.d/xdebug.ini && php vendor/phpunit/phpunit/phpunit --coverage-clover clover.xml --bootstrap modules/FramelixTests/tests/_bootstrap.php --configuration  modules/FramelixTests/tests/_phpunit.xml"
